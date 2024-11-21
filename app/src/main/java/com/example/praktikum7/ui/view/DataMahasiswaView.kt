cd96a02991e937b920fe2d2a3e26e1dc244fedbc
Namira Pramadhani <namnam6617@gmail.com>
2024-11-21 16:22:12 +0700

    menambahkan elevated button onbackclick dan horizontalAlignment

--- app/src/main/java/com/example/praktikum7/ui/view/DataMahasiswaView.kt
+++ app/src/main/java/com/example/praktikum7/ui/view/DataMahasiswaView.kt
@@ -3,10 +3,13 @@ package com.example.praktikum7.ui.view
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.praktikum7.model.DataMahasiswa
@@ -21,13 +24,23 @@ fun DataMahasiswaView(
         Pair("Nama", uiStateMahasiswa.nama),
         Pair("Gender", uiStateMahasiswa.gender),
         Pair("Alamat", uiStateMahasiswa.alamat),
+        Pair("NIM", uiStateMahasiswa.nim),
     )
 
-    Column() {
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         listDatamhs.forEach { items -> CardSection(
             judulParam = items.first,
             isiParam = items.second
         ) }
+        ElevatedButton(onClick = {onBackClick()}) {
+            Text(text = "Kembali")
+        }
     }
 }
 
