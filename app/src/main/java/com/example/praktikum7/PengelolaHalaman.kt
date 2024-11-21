3cd47fb7d80918fa3a156ed2a90bb14c259fa108
Namira Pramadhani <namnam6617@gmail.com>
2024-11-21 11:13:57 +0700

    Menambahkan parameter navController

--- app/src/main/java/com/example/praktikum7/PengelolaHalaman.kt
+++ app/src/main/java/com/example/praktikum7/PengelolaHalaman.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
@@ -20,6 +21,9 @@ enum class Halaman {
 }
 
 @Composable
-fun PengelolaHalaman(){
+fun PengelolaHalaman(
+    navController: NavHostController = rememberNavController(),
+
+){
 
 }
